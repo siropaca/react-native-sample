97b88d59109649f91fcb08f958f2d812a3ab9fce
siropaca <ryhshr3@gmail.com>
2020-12-10 21:15:04 +0900

    add alert

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ import {
   View,
   Text,
   StatusBar,
+  Button,
 } from 'react-native';
 
 import {
@@ -39,8 +40,14 @@ const App: () => React$Node = () => {
             </View>
           )}
           <View style={styles.body}>
+            <Button
+              title="Click Me"
+              onPress={() => {
+                alert('アラート表示');
+              }}
+            />
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One!!!????</Text>
+              <Text style={styles.sectionTitle}>Step One</Text>
               <Text style={styles.sectionDescription}>
                 Edit <Text style={styles.highlight}>App.js</Text> to change this
                 screen and then come back to see your edits.
